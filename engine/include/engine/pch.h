1cb1ed4e57f9511505ed4de64c0a7532ce372afd
allogic <mial.burg@gmail.com>
2026-01-16 19:29:05 +0100

    added basic project structure

---
+++ engine/include/engine/pch.h
@@ -0,0 +1,12 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+#endif // PCH_H
---
+++ engine/src/main.c
@@ -0,0 +1,3 @@
+int32_t main(int32_t argc, char **argv) {
+  return 0;
+}
